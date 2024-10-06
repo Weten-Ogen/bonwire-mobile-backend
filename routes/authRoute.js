a44d46d16305a11ef360b927dbf63b8f8c1f5eae
Weten-Ogen <alphaoware@gmail.com>
2024-10-06 01:24:58 +0000

    first commit

--- routes/authRoute.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require('express');
-const {login,register} = require("../controllers/authcontroller.js")
-const router = Router();
-
-router.post('/register', register)
-router.post('/login', login)
-
-module.exports = router

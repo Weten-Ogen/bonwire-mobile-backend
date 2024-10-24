440ef872b158d8c996e164de9628decc2739700f
Weten-Ogen <alphaoware@gmail.com>
2024-10-24 01:37:29 +0000

    fix prisma client error

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ const app = express()
 // middlewares 
 app.use(express.json())
 app.use(cors())
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(session({
-    secret: process.env.SECRET_KEY || '',
-    resave: false,
-    saveUninitialized: true,
+// app.use(cookieParser())
+// app.use(express.static(path.join(__dirname, 'public')))
+// app.use(session({
+//     secret: process.env.SECRET_KEY || '',
+//     resave: false,
+//     saveUninitialized: true,
     
-}))
+// }))
 
 // routes
 app.get('/', (req,res) => {

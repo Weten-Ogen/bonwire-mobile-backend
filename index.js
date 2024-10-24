15b1fe38e7f321b0b3b40cb7db26f79e280deda4
Weten-Ogen <alphaoware@gmail.com>
2024-10-24 11:11:04 +0000

    fixing prisma bugs

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express()
 // middlewares 
 app.use(express.json())
 app.use(cors())
-app.use(cookieParser())
+
 // app.use(express.static(path.join(__dirname, 'public')))
 // app.use(session({
 //     secret: process.env.SECRET_KEY || '',

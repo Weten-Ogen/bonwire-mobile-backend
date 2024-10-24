2dd65528136227cda87f3ce8131aab42157997f3
Weten-Ogen <alphaoware@gmail.com>
2024-10-24 11:39:36 +0000

    fixing prisma bugs

--- index.js
+++ index.js
@@ -13,15 +13,14 @@ const app = express()
 
 
 // middlewares 
+app.use(cookieParser())
 app.use(express.json())
 app.use(cors())
-app.use(cookieParser())
 app.use(session({
     secret: process.env.SECRET_KEY ,
     resave: false,
     saveUninitialized: true,
-    cookie: {secure:true}
-    
+    cookie: {secure:true}  
 }))
 
 // routes

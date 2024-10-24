facdf46b351ab845dd6f981d5110aff7984e209b
Weten-Ogen <alphaoware@gmail.com>
2024-10-24 11:16:33 +0000

    fixing prisma bugs

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const dotenv = require('dotenv')
 const authRouter= require('./routes/auth.js')
 const cors  = require('cors')
 const productRouter=require('./routes/product.js')
-const cookieParser= require('cookie-parser')
 const path =  require('path')
 const session = require("express-session")
 // const authMiddleware = require('./middleware.js');
@@ -17,11 +16,12 @@ const app = express()
 // middlewares 
 app.use(express.json())
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
 app.use(session({
-    secret: process.env.SECRET_KEY || '',
+    secret: process.env.SECRET_KEY ,
     resave: false,
     saveUninitialized: true,
+    cookie: {secure:true}
     
 }))
 

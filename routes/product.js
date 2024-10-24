571813cd23aea26e7854ab708f52f80b64f478af
Weten-Ogen <alphaoware@gmail.com>
2024-10-24 11:48:22 +0000

    fixing prisma bugs

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { createProduct, deleteProductById, getProduct, getProductById, updateProductById }  = require('../controllers/product.js')
 const authMiddleware=  require('../middleware.js');
-
+const app = express()
 app.use(authMiddleware())
 
 // get the router

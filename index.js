891daaa87f610a618d15ca379ebf5b2de7170ce5
Weten-Ogen <alphaoware@gmail.com>
2024-10-24 11:19:29 +0000

    fixing prisma bugs

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const dotenv = require('dotenv')
 const authRouter= require('./routes/auth.js')
 const cors  = require('cors')
 const productRouter=require('./routes/product.js')
-const path =  require('path')
 const session = require("express-session")
 // const authMiddleware = require('./middleware.js');
 

efa3492146c082d47b55474a88391c17742454d6
Weten-Ogen <alphaoware@gmail.com>
2024-10-24 01:57:15 +0000

    fix prisma client error

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const dotenv = require('dotenv')
 const authRouter= require('./routes/auth.js')
 const cors  = require('cors')
 const productRouter=require('./routes/product.js')
-// const cookieParser= require('cookie-parser')
+const cookieParser= require('cookie-parser')
 const path =  require('path')
-// const session = require("express-session")
+const session = require("express-session")
 
 
 
@@ -17,18 +17,18 @@ const app = express()
 // middlewares 
 app.use(express.json())
 app.use(cors())
-// app.use(cookieParser())
-// app.use(express.static(path.join(__dirname, 'public')))
-// app.use(session({
-//     secret: process.env.SECRET_KEY || '',
-//     resave: false,
-//     saveUninitialized: true,
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(session({
+    secret: process.env.SECRET_KEY || '',
+    resave: false,
+    saveUninitialized: true,
     
-// }))
+}))
 
 // routes
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname,'public', 'index.html'))
+app.get("/",(req,res) =>{
+    res.send("hello world")
 })
 
 app.use('/auth',authRouter)
